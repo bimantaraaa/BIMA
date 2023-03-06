9d1cc649504ae4bdc533fc8b71768fe8937afdee
bimantaraaa <bimantaraa9090@gmail.com>
2023-03-06 00:09:58 -0800

    lilik

---
+++ resources/views/home-page.blade.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>DRYME - Free Laundry Service Website Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Google Web Fonts -->
+    <link rel="stylesheet" href="//cdn.datatables.net/1.13.2/css/jquery.dataTables.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;800&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Topbar Start -->
+    <div class="container-fluid bg-primary py-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-lg-left mb-2 mb-lg-0">
+                    <div class="d-inline-flex align-items-center">
+                        <a class="text-white pr-3" href=""></a>
+                        <span class="text-white"></span>
+                        <a class="text-white px-3" href=""></a>
+                        <span class="text-white"></span>
+                        <a class="text-white pl-3" href=""></a>
+                    </div>
+                </div>
+                <div class="col-md-6 text-center text-lg-right">
+                    <div class="d-inline-flex align-items-center">
+                        <a class="text-white px-3" href="">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a class="text-white px-3" href="">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a class="text-white px-3" href="">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                        <a class="text-white px-3" href="">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                        <a class="text-white pl-3" href="">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <div class="container-fluid position-relative nav-bar p-0">
+        <div class="container-lg position-relative p-0 px-lg-3" style="z-index: 9;">
+            <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0 pl-3 pl-lg-5">
+                <a href="" class="navbar-brand">
+                    <h1 class="m-0 text-secondary"><span class="text-primary">L A U N D R Y B I M A N T A R A</h1>
+                </a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
+                    <div class="navbar-nav ml-auto py-0">
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navbar End -->
+
+
+    <!-- Carousel Start -->
+    <div class="container-fluid p-0">
+        <div id="header-carousel" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="w-100" src="img/carousel-1.jpg" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <div class="p-3" style="max-width: 900px;">
+                            <h4 class="text-white text-uppercase mb-md-3">Laundry & Dry Cleaning</h4>
+                            <h1 class="display-3 text-white mb-md-4">Best For Laundry Services</h1>
+                            <a href="" class="btn btn-primary py-md-3 px-md-5 mt-2">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="w-100" src="img/carousel-2.jpg" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <div class="p-3" style="max-width: 900px;">
+                            <h4 class="text-white text-uppercase mb-md-3">Laundry & Dry Cleaning</h4>
+                            <h1 class="display-3 text-white mb-md-4">Highly Professional Staff</h1>
+                            <a href="" class="btn btn-primary py-md-3 px-md-5 mt-2">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#header-carousel" data-slide="prev">
+                <div class="btn btn-secondary" style="width: 45px; height: 45px;">
+                    <span class="carousel-control-prev-icon mb-n2"></span>
+                </div>
+            </a>
+            <a class="carousel-control-next" href="#header-carousel" data-slide="next">
+                <div class="btn btn-secondary" style="width: 45px; height: 45px;">
+                    <span class="carousel-control-next-icon mb-n2"></span>
+                </div>
+            </a>
+        </div>
+    </div>
+    <!-- Carousel End -->
+
+
+    <!-- Contact Info Start -->
+    <div class="container-fluid contact-info mt-5 mb-4">
+        <div class="container" style="padding: 0 30px;">
+            <div class="row">
+                <div class="col-md-4 d-flex align-items-center justify-content-center bg-secondary mb-4 mb-lg-0"
+                    style="height: 100px;">
+                    <div class="d-inline-flex">
+                        <i class="fa fa-2x fa-map-marker-alt text-white m-0 mr-3"></i>
+                        <div class="d-flex flex-column">
+                            <h5 class="text-white font-weight-medium">Lokasi Kami</h5>
+                            <p class="m-0 text-white">Jl Keadilan no.34 jakarta timur</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 d-flex align-items-center justify-content-center bg-primary mb-4 mb-lg-0"
+                    style="height: 100px;">
+                    <div class="d-inline-flex text-left">
+                        <i class="fa fa-2x fa-envelope text-white m-0 mr-3"></i>
+                        <div class="d-flex flex-column">
+                            <h5 class="text-white font-weight-medium">Email Kami</h5>
+                            <p class="m-0 text-white">info@example.com</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 d-flex align-items-center justify-content-center bg-secondary mb-4 mb-lg-0"
+                    style="height: 100px;">
+                    <div class="d-inline-flex text-left">
+                        <i class="fa fa-2x fa-phone-alt text-white m-0 mr-3"></i>
+                        <div class="d-flex flex-column">
+                            <h5 class="text-white font-weight-medium">Panggilan Kami</h5>
+                            <p class="m-0 text-white">+012 345 6789</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact Info End -->
+
+
+    <!-- About Start -->
+    <div class="container-fluid py-5" id="about">
+        <div class="container pt-0 pt-lg-4">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <h6 class="text-secondary text-uppercase text-center font-weight-medium mb-3">Cari Pesanan</h6>
+                    <h1 class="display-4 text-center mb-5">Data Pesanan</h1>
+                </div>
+                <div class="col-10 p-3" style="background-color: #f1f1f1">
+                    <table class="table table-bordered table-stripped" id="tableKu">
+                        <thead>
+                            <tr style="background-color: #d38714; color: white;">
+                                <th>No</th>
+                                <th>Kode Invoice</th>
+                                <th>Pelanggan</th>
+                                <th>Outlet</th>
+                                <th>Total Harga</th>
+                                <th>Status</th>
+                                <th>Dibayar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($transaksi as $trx)
+                            <tr>
+                                <td scope="row">{{ $loop->iteration }}</td>
+                                <td>{{ $trx->kode_invoice }}</td>
+                                <td>{{ $trx->member->nama }}</td>
+                                <td>{{ $trx->outlet->nama }}</td>
+                                <td>Rp. {{ number_format($trx->total_harga,0,',','.') }}</td>
+
+                                <td>
+                                    @if ($trx->status == 'baru')
+                                    <span class="badge text-primary py-1 px-2"
+                                        style="font-size: 13px; border-radius: 20px; background-color: #c8c1ff">{{$trx->status}}</span>
+                                    @endif
+                                    @if ($trx->status == 'proses')
+                                    <span class="badge text-warning py-1 px-2"
+                                        style="font-size: 13px; border-radius: 20px; background-color: #fdff9b">{{$trx->status}}</span>
+                                    @endif
+                                    @if ($trx->status == 'diambil')
+                                    <span class="badge py-1 px-2"
+                                        style="font-size: 13px; border-radius: 20px; background-color: #ffdfc2; color: #ff9100">{{$trx->status}}</span>
+                                    @endif
+                                    @if ($trx->status == 'selesai')
+                                    <span class="badge text-success py-1 px-2"
+                                        style="font-size: 13px; border-radius: 20px; background-color: #b4ffa4">{{$trx->status}}</span>
+                                    @endif
+                                </td>
+                                <td>
+                                    @if ($trx->dibayar=='dibayar')
+                                    <span class="badge text-success py-1 px-2"
+                                        style="font-size: 13px; border-radius: 20px; background-color: #b4ffa4">{{$trx->dibayar}}</span>
+                                    @else()
+                                    <span class="badge text-danger py-1 px-2"
+                                        style="font-size: 13px; border-radius: 20px; background-color: #ffd9d9">{{$trx->dibayar}}</span>
+                                    @endif
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- About End -->
+
+
+            <!-- Services Start -->
+            <div class="container-fluid pt-5 pb-3" id="service">
+                <div class="container">
+                    <h6 class="text-secondary text-uppercase text-center font-weight-medium mb-3">LAYANAN KAMI</h6>
+                    <h1 class="display-4 text-center mb-5">Apa yang kita tawarkan</h1>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 pb-1">
+                            <div class="d-flex flex-column align-items-center justify-content-center text-center bg-light mb-4 px-4"
+                                style="height: 300px;">
+                                <div class="d-inline-flex align-items-center justify-content-center bg-white shadow rounded-circle mb-4"
+                                    style="width: 100px; height: 100px;">
+                                    <i class="fa fa-3x fa-cloud-sun text-secondary"></i>
+                                </div>
+                                <h4 class="font-weight-bold m-0">Dry Cleaning</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 pb-1">
+                            <div class="d-flex flex-column align-items-center justify-content-center text-center bg-light mb-4 px-4"
+                                style="height: 300px;">
+                                <div class="d-inline-flex align-items-center justify-content-center bg-white shadow rounded-circle mb-4"
+                                    style="width: 100px; height: 100px;">
+                                    <i class="fas fa-3x fa-soap text-secondary"></i>
+                                </div>
+                                <h4 class="font-weight-bold m-0">Wash & Laundry</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 pb-1">
+                            <div class="d-flex flex-column align-items-center justify-content-center text-center bg-light mb-4 px-4"
+                                style="height: 300px;">
+                                <div class="d-inline-flex align-items-center justify-content-center bg-white shadow rounded-circle mb-4"
+                                    style="width: 100px; height: 100px;">
+                                    <i class="fa fa-3x fa-burn text-secondary"></i>
+                                </div>
+                                <h4 class="font-weight-bold m-0">Curtain Laundry</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 pb-1">
+                            <div class="d-flex flex-column align-items-center justify-content-center text-center bg-light mb-4 px-4"
+                                style="height: 300px;">
+                                <div class="d-inline-flex align-items-center justify-content-center bg-white shadow rounded-circle mb-4"
+                                    style="width: 100px; height: 100px;">
+                                    <i class="fa fa-3x fa-tshirt text-secondary"></i>
+                                </div>
+                                <h4 class="font-weight-bold m-0">Suits Cleaning</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Services End -->
+
+
+            <!-- Features Start -->
+            <div class="container-fluid py-5">
+                <div class="container">
+                    <div class="row">
+            </div>
+            <!-- Features End -->
+
+
+            <!-- Working Process Start -->
+            <div class="container-fluid pt-5">
+                <div class="container">
+                    <h6 class="text-secondary text-uppercase text-center font-weight-medium mb-3">PROSES KERJA</h6>
+                    <h1 class="display-4 text-center mb-5">Bagaimana Kita Bekerja</h1>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6">
+                            <div class="d-flex flex-column align-items-center justify-content-center text-center mb-5">
+                                <div class="d-inline-flex align-items-center justify-content-center bg-white border border-light shadow rounded-circle mb-4"
+                                    style="width: 150px; height: 150px; border-width: 15px !important;">
+                                    <h2 class="display-2 text-secondary m-0">1</h2>
+                                </div>
+                                <h3 class="font-weight-bold m-0 mt-2">Tempat Pesanan</h3>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="d-flex flex-column align-items-center justify-content-center text-center mb-5">
+                                <div class="d-inline-flex align-items-center justify-content-center bg-white border border-light shadow rounded-circle mb-4"
+                                    style="width: 150px; height: 150px; border-width: 15px !important;">
+                                    <h2 class="display-2 text-secondary m-0">2</h2>
+                                </div>
+                                <h3 class="font-weight-bold m-0 mt-2">Penjemputan Gratis</h3>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="d-flex flex-column align-items-center justify-content-center text-center mb-5">
+                                <div class="d-inline-flex align-items-center justify-content-center bg-white border border-light shadow rounded-circle mb-4"
+                                    style="width: 150px; height: 150px; border-width: 15px !important;">
+                                    <h2 class="display-2 text-secondary m-0">3</h2>
+                                </div>
+                                <h3 class="font-weight-bold m-0 mt-2">Cuci Kering</h3>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="d-flex flex-column align-items-center justify-content-center text-center mb-5">
+                                <div class="d-inline-flex align-items-center justify-content-center bg-white border border-light shadow rounded-circle mb-4"
+                                    style="width: 150px; height: 150px; border-width: 15px !important;">
+                                    <h2 class="display-2 text-secondary m-0">4</h2>
+                                </div>
+                                <h3 class="font-weight-bold m-0 mt-2">Pengiriman Gratis</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Working Process End -->
+
+
+            <!-- Pricing Plan Start -->
+           
